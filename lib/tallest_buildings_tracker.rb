2a034dc3220701e6bf017ae396e7884d87743890
MarkBezsonov <markbezsonov@gmail.com>
2021-03-01 06:57:02 -0600

    Added ascii_skyscraper_image class for cleaner access to the skyscraper art

--- lib/tallest_buildings_tracker.rb
+++ lib/tallest_buildings_tracker.rb
@@ -2,10 +2,12 @@ require_relative "./tallest_buildings_tracker/version"
 require_relative "./tallest_buildings_tracker/cli"
 require_relative "./tallest_buildings_tracker/info"
 require_relative "./tallest_buildings_tracker/scraper"
+require_relative "./tallest_buildings_tracker/ascii_skyscraper_image"
 
 require 'pry'
 require 'nokogiri'
 require 'open-uri'
+require 'colorize'
 
 module TallestBuildingsTracker
   class Error < StandardError; end
